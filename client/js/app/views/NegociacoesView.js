1d38d0a7ee685415324efe10599a2e9246fbad48
Thiago Resende <thiago.gcresende@gmail.com>
2018-03-12 09:59:04 -0300

    View render

--- client/js/app/views/NegociacoesView.js
+++ client/js/app/views/NegociacoesView.js
@@ -5,11 +5,12 @@ class NegociacoesView {
         this._elemento = elemento;
     }
 
-    update() {
-        this._elemento.innerHTML = this._template();
+    update(model) {
+
+        this._elemento.innerHTML = this._template(model);
     }
 
-    _template() {
+    _template(model) {
 
         return `
         <table class="table table-hover table-bordered">
@@ -22,8 +23,22 @@ class NegociacoesView {
                 </tr>
             </thead>
             <tbody>
+            ${model.negociacoes.map(n =>
+                `<tr>
+                        <td>${DateHelper.dataParaTexto(n.data)}</td>
+                        <td>${n.quantidade}</td>
+                        <td>${n.valor}</td>
+                        <td>${n.volume}</td>
+                    </tr>
+                `).join('')}
             </tbody>
             <tfoot>
+                <tr>
+                    <td colspan="3"></td>
+                    <td>
+                    ${ model.negociacoes.reduce((total, n) => total + n.volume, 0)}
+                    </td>
+                </tr>
             </tfoot>
         </table>`;
     }

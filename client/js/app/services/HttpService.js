756cd358230c23f6c3a1fbc6ccecfed0a8bc1abc
Thiago Resende <thiago.gcresende@gmail.com>
2018-03-22 16:23:41 -0300

    Fetch API

--- client/js/app/services/HttpService.js
+++ client/js/app/services/HttpService.js
@@ -1,19 +1,17 @@
 class HttpService {
 
+    _handleErrors(res) {
+        if (res.ok) {
+            return res;
+        } else {
+            throw new Error(res.statusText);
+        }
+    }
+
     get(url) {
-        return new Promise((resolve, reject) => {
-            let xhr = new XMLHttpRequest();
-            xhr.open('GET', url);
-            xhr.onreadystatechange = () => {
-                if (xhr.readyState == 4) {
-                    if (xhr.status == 200) {
-                        resolve(JSON.parse(xhr.responseText))
-                    } else {
-                        reject(xhr.responseText);
-                    }
-                }
-            };
-            xhr.send();
-        });
+
+        return fetch(url)
+            .then(res => this._handleErrors(res))
+            .then(res => res.json());
     }
 }
